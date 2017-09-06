c40291df2d75d8ab5fd8bf853a1720871abaad10
zhuxiangpeng <zxpwork@gmail.com>
2017-09-06 15:26:16 +0800

    添加Example

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by zhuxiangpeng on 2017/9/6.
+//  Copyright © 2017年 xpz. All rights reserved.
+//
+
+import UIKit
+import DataDominantTableView
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		let _ = DDTableViewCellModel()
+		
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
